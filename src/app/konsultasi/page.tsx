95c6dfe17bbdd299130e4fce4c5fc5c8d55d5800
AlgebraIbrahim <algebraibrahim@gmail.com>
2025-07-02 22:40:07 +0700

    add konsultasi dan registrasi pages

---
+++ src/app/konsultasi/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Konsultasi() {
+    return (
+        <div>Konsultasi</div>
+    )
+}
---
+++ src/app/registrasi/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Registrasi() {
+    return (
+        <div>Registrasi</div>
+    )
+}
